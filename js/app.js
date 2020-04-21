25d2b47e6b4c2400186d1efb4df27763ae4cbd98
Cader <mcader@hasnatech.com>
2020-04-21 13:02:07 +0530

    added five images

--- js/app.js
+++ js/app.js
@@ -1,50 +1,79 @@
-jQuery(document).ready(function($) {
-    
+jQuery(document).ready(function ($) {
     slider($);
-    
 })
 
-function slider($){
+function slider($) {
     var num = 0;
 
-    $(".sliderText .title").each(function(){
+    $(".sliderText .title").each(function () {
         $(".slider-panel .indicator").append("<div class='box'></div>");
     })
+
+    $(".slider .indicator").css("left", "calc(50% - " + ($(".slider .indicator").width()/2) + "px)")
+
     $(".sliderText .title").hide();
     $(".sliderText .textwidget").hide();
-    
+    $(".sliderText .img").animate({
+        opacity: 0,
+    }, 0);
+
 
     $(".sliderText .title").eq(num).show();
     $(".sliderText .title").eq(num).animate({
         opacity: 1,
         top: -50
     }, 500);
+
     $(".sliderText .textwidget").eq(num).show();
     $(".sliderText .textwidget").eq(num).animate({
         opacity: 1,
         top: -50
-    }, 750)
+    }, 750);
+
+    $(".sliderText .img").eq(num).show();
+    $(".sliderText .img").eq(num).animate({
+        opacity: 1,
+    }, 250);
+
+
     $(".slider-panel .indicator .box").eq(num).addClass("selected");
-    setInterval(function(){
+    setInterval(function () {
         num = num + 1;
         num = num % $(".sliderText .title").length;
 
         $(".sliderText .title").animate({
             opacity: 0,
             top: 0
-        }, 500, function(){
+        }, 500, function () {
             $(this).hide();
         });
         $(".sliderText .textwidget").animate({
             opacity: 0,
             top: 0
-        }, 500, function(){
+        }, 500, function () {
             $(this).hide();
         });
+        var temp_n = 0;
+        $(".sliderText .img").each(function () {
+            temp_n++;
+            if(temp_n != num){
+                $(this).animate({
+                    opacity: 0
+                }, 500, function () {
+                    //$(this).hide();
+                });
+            }
+        })
+
+        $(".sliderText .img").eq(num).show();
+        $(".sliderText .img").eq(num).animate({
+            opacity: 1,
+        }, 500);
+
         $(".slider-panel .indicator .box").removeClass("selected");
         $(".slider-panel .indicator .box").eq(num).addClass("selected");
         console.log(num)
-        setTimeout(function(){
+        setTimeout(function () {
             $(".sliderText .title").eq(num).show();
             $(".sliderText .title").eq(num).animate({
                 opacity: 1,
@@ -55,7 +84,8 @@ function slider($){
                 opacity: 1,
                 top: -50
             }, 750)
-            
+
+
         }, 750)
     }, 5000);
 }
